8aa60851845997b9426da4f58e8828d2dcb1196b
DRN-X2 <xvr09200@gmail.com>
2026-04-20 08:33:58 +0800

    Added a photo to card

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -27,7 +27,7 @@ const Feed: React.FC = () => {
           {games.map((item, index) => (
             <IonItem>
                <IonCard>
-                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                  <img alt="Silhouette of mountains" src="https://imgs.search.brave.com/4-rEEUuA7Ex2POouYoAo2h39pvhNpfEXB5sqyfU41F8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG9rZW1vbi5jb20v/c3RhdGljLWFzc2V0/cy9jb250ZW50LWFz/c2V0cy9jbXMyL2lt/Zy9wb2tlZGV4L2Z1/bGwvMzg0X2YyLnBu/Zw" />
                     <IonCardHeader>
                        <IonCardTitle>{item.name}</IonCardTitle>
                            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
