9349b3c6d6f9560862da8e295f043ab27878271c
DRN-X2 <xvr09200@gmail.com>
2026-04-20 08:16:05 +0800

    Added items for list

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,22 +1,47 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { chatboxEllipsesOutline, shareOutline, thumbsUpOutline} from 'ionicons/icons';
 
 const Feed: React.FC = () => {
+
+  const games = [
+    {name: 'Pokemon Yellow'},
+    {name: 'Pokemon Red'},
+    {name: 'Pokemon Blue'}
+  ]
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
-            <IonMenuButton />
+            <IonMenuButton></IonMenuButton>
+            <IonTitle>Feed</IonTitle>
           </IonButtons>
-          <IonTitle>Feed</IonTitle>
+
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent className="ion-padding">
+        <IonList inset={true}>
+
+          {games.map((item, index) => (
+            <IonItem>
+               <IonCard>
+                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                    <IonCardHeader>
+                       <IonCardTitle>{item.name}</IonCardTitle>
+                           <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                    </IonCardHeader>
+
+                       <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+               </IonCard>
+            </IonItem>
+
+
+          ))}
+
+
+        </IonList>
       </IonContent>
     </IonPage>
   );
